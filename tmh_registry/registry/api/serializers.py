d8de168b17acbc9945c875dace37e7d8cb6728c1
Dimitris Koutsonikolis <koutsonikolis@orfium.com>
2021-06-04 19:13:27 +0300

    feat(hospital): added hospital serializer

---
+++ tmh_registry/registry/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from ..models import Hospital
+
+
+class HospitalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hospital
+        fields = ["name", "address"]
