868f9e2b5de3d7722366a1b39c97d1b7c43d4171
Stefanos Doulfis <stefanos@orfium.com>
2021-06-04 17:32:25 +0300

    fix admin error

--- tmh_registry/users/admin.py
+++ tmh_registry/users/admin.py
@@ -6,4 +6,3 @@ from tmh_registry.users.models import MedicalPersonnel
 @admin.register(MedicalPersonnel)
 class MedicalPersonnelAdmin(admin.ModelAdmin):
     model = MedicalPersonnel
-    list_display = ("__all__",)
