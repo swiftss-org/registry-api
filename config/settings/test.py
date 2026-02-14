6d8fc50ad6f7150f42a8a94a68af061181582665
Sabi <szigyi.szogyi@gmail.com>
2026-02-14 14:48:04 +0000

    remove md5 hasher

--- config/settings/test.py
+++ config/settings/test.py
@@ -29,7 +29,6 @@ CACHES = {
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
 PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.MD5PasswordHasher"
     "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
